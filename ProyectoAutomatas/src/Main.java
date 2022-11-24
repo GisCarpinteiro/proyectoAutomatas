615e38d60aa3019cb295814dddcd89285c073f72
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2022-11-23 19:47:35 -0600

    KILL ME PLS

---
+++ ProyectoAutomatas/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author David Alejando Gonzalez Quezada
+ * @project ${PROJECT_NAME}
+ * ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        Form form = new Form();
+        form.setVisible(true);
+        System.out.println("Hello world!");
+    }
+}
